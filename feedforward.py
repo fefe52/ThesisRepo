cf74440c54a508d7e64f1461e991e01b42ea6df2
fefe52 <fedearesu52@hotmail.com>
2020-12-17 16:19:26 +0000

    check over 1500 epochs test06

--- feedforward.py
+++ feedforward.py
@@ -118,7 +118,7 @@ def main():
 
     # Create the model
     model = Sequential()
-    model.add(Dense(400,kernel_regularizer=regularizers.l2(0.001),  activation='relu',input_dim=n_input))
+    model.add(Dense(500,kernel_regularizer=regularizers.l2(0.001),  activation='relu',input_dim=n_input))
     model.add(Dropout(.2))
     model.add(Dense(100,kernel_regularizer=regularizers.l2(0.001), activation='relu'))
     model.add(Dense(50, activation = 'relu'))
@@ -146,7 +146,7 @@ def main():
 
     # Early stopping
     #es = EarlyStopping(monitor='val_loss', mode='min', min_delta = 0.01, baseline=None)   
-    my_callback = MyThresholdCallback(threshold=0.20)
+    my_callback = MyThresholdCallback(threshold=0.10)
     # validation_split=0.2 TO USE
     model_history = model.fit(train_features, train_target, validation_data=(val_features,val_target), epochs=1500, batch_size = len(train_target), verbose=1, callbacks = [my_callback])
     ### to plot model's training cost/loss and model's validation split cost/loss
